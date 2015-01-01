fbe786cdd155dc873a82325010d97450977a322d
wayou <liuwayong@gmail.com>
2015-01-01 14:36:04 +0800

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngmaterialApp
+ * @description
+ * # ngmaterialApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngmaterialApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngmaterialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngmaterialApp
+ */
+angular.module('ngmaterialApp')
+    .controller('MainCtrl', function($scope, $mdDialog,$interval) {
+
+        $scope.title1 = 'Button';
+        $scope.title4 = 'Warn';
+        $scope.isDisabled = true;
+        $scope.googleUrl = 'http://google.com';
+
+        $scope.data = {};
+        $scope.data.cb1 = true;
+        $scope.data.cb2 = false;
+        $scope.data.cb3 = false;
+        $scope.data.cb4 = false;
+        $scope.data.cb5 = false;
+
+        $scope.mode = 'query';
+        $scope.determinateValue = 30;
+        $interval(function() {
+            $scope.determinateValue += 1;
+            if ($scope.determinateValue > 100) {
+                $scope.determinateValue = 30;
+            }
+        }, 100, 0, true);
+
+        $scope.alert = '';
+        $scope.showAlert = function(ev) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                .title('This is an alert title')
+                .content('You can specify some description text in here.')
+                .ariaLabel('Password notification')
+                .ok('Got it!')
+                .targetEvent(ev)
+            );
+        };
+        $scope.showConfirm = function(ev) {
+            var confirm = $mdDialog.confirm()
+                .title('Would you like to delete your debt?')
+                .content('All of the banks have agreed to forgive you your debts.')
+                .ariaLabel('Lucky day')
+                .ok('Please do it!')
+                .cancel('Sounds like a scam')
+                .targetEvent(ev);
+            $mdDialog.show(confirm).then(function() {
+                $scope.alert = 'You decided to get rid of your debt.';
+            }, function() {
+                $scope.alert = 'You decided to keep your debt.';
+            });
+        };
+        $scope.showAdvanced = function(ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: '../views/dialog1.tmpl.html',
+                targetEvent: ev,
+            })
+                .then(function(answer) {
+                    $scope.alert = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.alert = 'You cancelled the dialog.';
+                });
+        };
+    });
+
+function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function() {
+        $mdDialog.cancel();
+    };
+    $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+    };
+}
